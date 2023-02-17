64e3a83d3bf9ebca59bae4a55cd380c290df79b1
Vahid Akhtar <akhtarvahid543@gmail.com>
2023-02-17 18:07:15 +0530

    clothing markup created

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -3,8 +3,42 @@ import './App.css';
 
 const App = () => {
   return (
-    <div className="App">
-      Hello world
+    <div className="categories-container">
+      <div className='category-container'>
+        {/* img */}
+        <div className='category-body-container'>
+         <h2>Hats</h2>
+         <p>Shop Now</p>
+        </div>
+      </div>
+      <div className='category-container'>
+        {/* img */}
+        <div className='category-body-container'>
+         <h2>Jackets</h2>
+         <p>Shop Now</p>
+        </div>
+      </div>
+      <div className='category-container'>
+        {/* img */}
+        <div className='category-body-container'>
+         <h2>Sneakers</h2>
+         <p>Shop Now</p>
+        </div>
+      </div>
+      <div className='category-container'>
+        {/* img */}
+        <div className='category-body-container'>
+         <h2>Womens</h2>
+         <p>Shop Now</p>
+        </div>
+      </div>
+      <div className='category-container'>
+        {/* img */}
+        <div className='category-body-container'>
+         <h2>Mens</h2>
+         <p>Shop Now</p>
+        </div>
+      </div>
     </div>
   );
 }
