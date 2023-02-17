a487b9d6af6ba8e9c7274ab12061e62d5686dee1
Vahid Akhtar <akhtarvahid543@gmail.com>
2023-02-17 18:32:52 +0530

    clothing markup iterated to reduce html markups

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -2,43 +2,37 @@ import React from 'react';
 import './App.css';
 
 const App = () => {
+  const categories = [{
+    id: 1,
+    title: 'Hats'
+  },
+  {
+    id: 2,
+    title: 'Jackets'
+  },
+  {
+    id: 3,
+    title: 'Sneakers'
+  },{
+    id: 4,
+    title: 'Womens'
+  },
+  {
+    id: 5,
+    title: 'Mens'
+  }
+  ]
   return (
     <div className="categories-container">
-      <div className='category-container'>
-        {/* img */}
+      {categories.map(category => 
+        <div className='category-container' key={category.id}>
+        <div className='background-image'/>
         <div className='category-body-container'>
-         <h2>Hats</h2>
+         <h2>{category.title}</h2>
          <p>Shop Now</p>
         </div>
       </div>
-      <div className='category-container'>
-        {/* img */}
-        <div className='category-body-container'>
-         <h2>Jackets</h2>
-         <p>Shop Now</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/* img */}
-        <div className='category-body-container'>
-         <h2>Sneakers</h2>
-         <p>Shop Now</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/* img */}
-        <div className='category-body-container'>
-         <h2>Womens</h2>
-         <p>Shop Now</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/* img */}
-        <div className='category-body-container'>
-         <h2>Mens</h2>
-         <p>Shop Now</p>
-        </div>
-      </div>
+        )}
     </div>
   );
 }
