8242fd2a92b89959c92221c00a6a351b2d8099b1
Vahid Akhtar <akhtarvahid543@gmail.com>
2023-02-18 18:12:56 +0530

    categories styling created

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import './App.css';
+// import './App.css';
 import data from './data.json'
+import './categories.styles.scss';
+
 const App = () => {
 
 
@@ -8,12 +10,12 @@ const App = () => {
     <div className="categories-container">
       {data.map(category => 
         <div className='category-container' key={category.id}>
-        <div className='background-image'/>
-        <div className='category-body-container'>
-         <h2>{category.title}</h2>
-         <p>Shop Now</p>
+          <div className='background-image'/>
+          <div className='category-body-container'>
+          <h2>{category.title}</h2>
+          <p>Shop Now</p>
+          </div>
         </div>
-      </div>
         )}
     </div>
   );
