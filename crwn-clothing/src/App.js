58d6882cf81347abda291e9bc7afc9b2cfb1f1fe
Vahid Akhtar <akhtarvahid543@gmail.com>
2023-02-22 18:26:06 +0530

    routing home, shop configured

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import './App.css';
-import { Route, Routes } from 'react-router-dom';
+import { Outlet, Route, Routes } from 'react-router-dom';
 import Home from './routes/Home.component';
 
+const Navigation = () => {
+  return (
+    <div>
+     <div>
+      <h1>I am the Navigation bar</h1> 
+     </div>
+     <Outlet />
+    </div>
+  )
+}
+const Shop = () => {
+  return <h1>I am the shop page</h1>;
+}
+
 const App = () => {
-
-
   return (
     <Routes>
-      <Route path='/' element={<Home />} />
+      <Route path='/' element={<Navigation />}>
+       <Route path='/home' element={<Home />} />
+       <Route path='/shop' element={<Shop />} />
+      </Route>
     </Routes>
   );
 }
