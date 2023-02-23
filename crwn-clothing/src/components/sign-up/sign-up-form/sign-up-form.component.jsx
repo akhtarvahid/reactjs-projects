7423df1dac01f52307c9c1cb3ad4c118f39e1dee
Vahid Akhtar <akhtarvahid543@gmail.com>
2023-02-24 00:52:00 +0530

    sign-up form created with fields

--- crwn-clothing/src/components/sign-up/sign-up-form/sign-up-form.component.jsx
+++ crwn-clothing/src/components/sign-up/sign-up-form/sign-up-form.component.jsx
@@ -1,21 +1,40 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+
+const defaultFormFields = {
+    displayName: '',
+    email: '',
+    password: '',
+    confirmPassword: ''
+}
 
 const SignUpForm = () => {
+  const [formFields, setFormFields] = useState(defaultFormFields);
+  const { displayName, email, password, confirmPassword } = formFields;
+  const handleChange = (e) => {
+     const { name, value} = e.target;
+     setFormFields({
+        ...formFields,
+        [name]: value
+     })
+  } 
+
+  console.log(formFields)
+
   return (
     <div>
       <h1>Sign up with your email and password</h1>
       <form onSubmit={() => {}}>
         <label>Display name</label> 
-        <input type="text" required/>
+        <input type="text" onChange={handleChange} value={displayName} name='displayName' required/>
         
         <label>Email</label> 
-        <input type="email" required/>
+        <input type="email"  onChange={handleChange} value={email} name='email' required/>
         
         <label>Password</label> 
-        <input type="password" required/>
+        <input type="password"  onChange={handleChange} value={password} name='password' required/>
         
         <label>Confirm Password</label> 
-        <input type="password" required/>
+        <input type="password" onChange={handleChange} value={confirmPassword} name='confirmPassword' required/>
 
         <button type='Submit'>Sign Up</button>
       </form>  
