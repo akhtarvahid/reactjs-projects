9090a6dde234c10523fa0e1c81d82d85cb80f59f
Vahid Akhtar <akhtarvahid543@gmail.com>
2023-03-03 16:57:12 +0530

    merged child folder

--- crwn-clothing/src/components/directory/directory.component.jsx
+++ crwn-clothing/src/components/directory/directory.component.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import CategoryItem from '../category-item/category-item.component';
+import CategoryItem from './category-item/category-item.component';
 import './directory.styles.scss';
 
 const Directory = ({ data }) => {
