898fd4ead1de09ab8e2cce129d3f3e7fdac6b073
Vahid Akhtar <akhtarvahid543@gmail.com>
2024-06-11 20:47:56 +0530

    clothin app

---
+++ clothing/src/components/form-input/form-input.components.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./form-input.styles.scss";
+
+const FormInput = ({ label, ...restProps }) => {
+  return (
+    <div className="group">
+      <input className="form-input" {...restProps} />
+      <label
+        className={`${restProps.value.length ? "shrink" : ""} form-input-label`}
+      >
+        {label}
+      </label>
+    </div>
+  );
+};
+export default FormInput;
