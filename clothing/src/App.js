c50ff2217bd2fcdeee9b88c47e15fba52e8bd56e
Vahid Akhtar <akhtarvahid543@gmail.com>
2024-06-14 19:35:20 +0530

    clothing: refactoring

--- clothing/src/App.js
+++ clothing/src/App.js
@@ -1,25 +1,24 @@
-import React from 'react';
-import './App.css';
-import { Route, Routes } from 'react-router-dom';
-import Home from './routes/home/Home.component';
-import Navigation from './routes/navigation/Navigation.component';
-import Authentication from './routes/authentication/authentication.component';
-
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./routes/home/Home.component";
+import Navigation from "./routes/navigation/Navigation.component";
+import Authentication from "./routes/authentication/authentication.component";
+import "./App.css";
 
 const Shop = () => {
   return <h1>I am the shop page</h1>;
-}
+};
 
 const App = () => {
   return (
     <Routes>
-      <Route path='/' element={<Navigation />}>
-       <Route index element={<Home />} />
-       <Route path='/shop' element={<Shop />} />
-       <Route path='/auth' element={<Authentication />} />
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/auth" element={<Authentication />} />
       </Route>
     </Routes>
   );
-}
+};
 
 export default App;
--- clothing/src/context/user.context.jsx
+++ clothing/src/context/user.context.jsx
@@ -1,24 +1,26 @@
-import React, { createContext, useState, useEffect } from 'react'
-import { createUserDocumentFromAuth, onAuthStateChangedListener } from '../utils/firebase/firebase.utils';
- 
+import React, { createContext, useState, useEffect } from "react";
+import {
+  createUserDocumentFromAuth,
+  onAuthStateChangedListener,
+} from "../utils/firebase";
 
 export const UserContext = createContext({
-     currentUser: null,
-     setCurrentUser: () => null,
+  currentUser: null,
+  setCurrentUser: () => null,
 });
 
 export const UserProvider = ({ children }) => {
-    const [currentUser, setCurrentUser] = useState(null);
-    const value = { currentUser, setCurrentUser }; 
-    useEffect(() => {
-       const unsubscribe = onAuthStateChangedListener((user) => {
-        if(user) {
-            createUserDocumentFromAuth(user);
-        }
-        setCurrentUser(user);
-       });
-       return unsubscribe();
-    }, []);
+  const [currentUser, setCurrentUser] = useState(null);
+  const value = { currentUser, setCurrentUser };
+  useEffect(() => {
+    const unsubscribe = onAuthStateChangedListener((user) => {
+      if (user) {
+        createUserDocumentFromAuth(user);
+      }
+      setCurrentUser(user);
+    });
+    return unsubscribe();
+  }, []);
 
-    return <UserContext.Provider value={value}>{children}</UserContext.Provider>
-}
+  return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
+};
